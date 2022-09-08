09c6d4b5de58b128792350bf615657f5da2c8c33
Venya Brodetskiy <benjamincheg@gmail.com>
2022-09-08 18:36:20 +0300

    MP-11: added getBoardType by ID

--- source/controllers/school.controller.ts
+++ source/controllers/school.controller.ts
@@ -9,7 +9,7 @@ const getBoardTypes = async (req: Request, res: Response, next: NextFunction) =>
     schoolService.getBoardTypes()
         .then((result: whiteBoardType[]) => {
             return res.status(200).json({
-                message: result
+                result
             });
         })
         .catch((error: systemError) => {
@@ -31,4 +31,49 @@ const getBoardTypes = async (req: Request, res: Response, next: NextFunction) =>
     
 };
 
-export default { getBoardTypes };
+const getBoardType = async (req: Request, res: Response, next: NextFunction) => {
+    let id: number = -1; // declare default value, which obviously cannot work
+
+    const sID: string = req.params.id;
+    if (isNaN(Number(sID))) {
+        // TODO: Error handling
+    }
+
+    if (sID !== null && sID !== undefined) {
+        id = parseInt(sID);
+    }
+    else {
+        // TODO: Error handling
+    }
+    
+    if (id > 0) {
+        schoolService.getBoardType(id)
+            .then((result: whiteBoardType) => {
+                return res.status(200).json({
+                    result
+                });
+            })
+            .catch((error: systemError) => {
+                switch (error.code) {
+                    case ErrorCodes.ConnectionError:
+                        return res.status(408).json({
+                            errorMessage: error.message
+                        });
+                    case ErrorCodes.QueryError:
+                        return res.status(406).json({
+                            errorMessage: error.message
+                        })
+                    default:
+                        return res.status(400).json({
+                            errorMessage: error.message
+                        });
+                }
+            })
+    }
+    else {
+        // TODO: Error handling
+    }
+    
+};
+
+export default { getBoardTypes, getBoardType };
