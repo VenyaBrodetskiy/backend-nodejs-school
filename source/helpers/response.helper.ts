a59d3f0b838c4243c7620264b53966c43b59cc90
Venya Brodetskiy <benjamincheg@gmail.com>
2022-09-11 13:12:23 +0300

    MP11: added response helpers

---
+++ source/helpers/response.helper.ts
@@ -0,0 +1,24 @@
+import { Response } from 'express';
+import { ErrorCodes } from '../constants';
+import { systemError } from "../entities";
+
+export class ResponseHelper {
+
+    public static handleError(response: Response, error: systemError): Response<any, Record<string, any>> {
+
+        switch (error.code) {
+            case ErrorCodes.ConnectionError:
+                return response.status(408).json({
+                    errorMessage: error.message
+                });
+            case ErrorCodes.QueryError:
+                return response.status(406).json({
+                    errorMessage: error.message
+                })
+            default:
+                return response.status(400).json({
+                    errorMessage: error.message
+                });
+        }
+    }
+}
