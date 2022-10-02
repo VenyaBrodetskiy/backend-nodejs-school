bff35897492ac93de6fc75cf79396bafd6f7629a
Venya Brodetskiy <benjamincheg@gmail.com>
2022-10-02 16:01:03 +0300

    minor

--- source/middleware/auth.middleware.ts
+++ source/middleware/auth.middleware.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Response, Request } from "express";
 import jwt, { JwtPayload } from "jsonwebtoken";
+import _ from "underscore";
 import { TOKEN_SECRET } from "../constants";
 import { AuthenticatedRequest, jwtUserData } from "../entities";
 import { Role } from "../enums";
@@ -19,6 +20,10 @@ const verifyToken = (roles: Role[]) => (req: Request, res: Response, next: NextF
     try {
         token = token.substring("Bearer ".length);
         const decoded: string | JwtPayload = jwt.verify(token, TOKEN_SECRET);
+        
+        // in case many-to-many connection of roles to user probably we will need this..
+        // if (_.any((decoded as jwtBase).userData.roles, (role: Role) => ))
+        
         if (roles.indexOf((decoded as jwtBase).userData.roleId) === -1) {
             return res.sendStatus(401);
         }
